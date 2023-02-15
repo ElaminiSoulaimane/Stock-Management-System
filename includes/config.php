e87ffaf75601cdf922000a8d56a019d7d2182ee5
ElaminiSoulaimane <soulaimane.elamini1@gmail.com>
2023-02-15 22:06:13 +0100

    V1

--- includes/config.php
+++ includes/config.php
@@ -1,13 +1,10 @@
 <?php
 /*
 |--------------------------------------------------------------------------
-| OWSA-INV V2
+|V1
 |--------------------------------------------------------------------------
-| Author: Siamon Hasan
-| Project Name: OSWA-INV
-| Version: v2
-| Offcial page: http://oswapp.com/
-| facebook Page: https://www.facebook.com/oswapp
+| Author: Soulaimane Elamini
+| Version: v1
 |
 |
 |
@@ -15,6 +12,6 @@
   define( 'DB_HOST', 'localhost' );          // Set database host
   define( 'DB_USER', 'root' );             // Set database user
   define( 'DB_PASS', '' );             // Set database password
-  define( 'DB_NAME', 'inventory_system' );        // Set database name
+  define( 'DB_NAME', 'Management_system' );        // Set database name
 
 ?>
